8919f4489414b9c3fbf4e773419a2a94ec2a6eb5
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-05-07 14:20:32 +0530

    Completed User interface

---
+++ script.js
@@ -0,0 +1,11 @@
+let elements=[
+    {
+        id:1,
+        image:"./assets/FB_IMG_1652607151685.jpg",
+        name: 'Sandeep Kumar',
+        job: "Frontend Developer",
+        tittle: " As a frontend developer is a proficient artisan of user interfaces, crafting digital experiences with precision and creativity. Armed with a keen eye for design and a mastery of HTML, CSS, and JavaScript, they sculpt seamless interactions that captivate and engage users, bringing your visions to life with finesse.",
+    },{
+
+    }
+]
